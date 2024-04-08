bccba2810694ba1e01907cb7ead2f6a1ab1cc560
Juan Jurado <juanjurado62@gmail.com>
2024-04-07 18:18:53 -0600

    añadida la base de datos con la nueva tabla de administradores

--- db/ujedsettingv6.sql
+++ db/ujedsettingv6.sql
@@ -18,6 +18,22 @@ USE `ujedsetting`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admins` (
+  `idadmin` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  PRIMARY KEY (`idadmin`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `alumnos`
 --
 
@@ -166,4 +182,4 @@ CREATE TABLE `tutorestesis` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-04 16:21:44
+-- Dump completed on 2024-04-07 18:16:23
